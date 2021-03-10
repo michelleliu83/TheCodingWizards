b56d0ccdb398302cf0369f16d5fc593418be72ad
michelleliu83 <79758219+michelleliu83@users.noreply.github.com>
2021-03-10 12:33:24 -0500

    Update heatmap_final.m

--- heatmap_final.m
+++ heatmap_final.m
@@ -1,8 +1,14 @@
-% portion completed by Laurel
+% Creating a "heat map" that shows where fatalities occurred in the United States
+
+% Writing and debugging by Laurel, 4 hours
+
+% Extracting necessary variables for analysis
 latitude = T(:, 16);
 longitude = T(:, 15);
 lat = table2array(latitude);
-long = table2array(longitude);
+long = table2array(longitude); 
+
+% Creating states to display data on
 states = geoshape(shaperead('usastatehi', 'UseGeoCoords', true));
 figure
 ax = usamap('conus');
