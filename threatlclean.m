e705f8dd6bbe5ce529f286106318b24746743332
michelleliu83 <79758219+michelleliu83@users.noreply.github.com>
2021-03-10 12:49:55 -0500

    Update threatlclean.m

--- threatlclean.m
+++ threatlclean.m
@@ -1,4 +1,4 @@
-% We removed 'undetermined' threat levels from the dataset
+% Removing 'undetermined' threat levels from the dataset for visualizations and data analysis
 
 threat_level_clean = table2cell(threat_level);
 idx = strcmp(threat_level_clean,'undetermined');
@@ -6,4 +6,5 @@ threat_level_clean(idx) = [];
 threat_level_clean 
 
 % Writing the code: Laurel, 1 hour
-% Debugging the code: Michelle, 1 hour
+% Debugging the code: Sarah, 1 hour
+% Collaborated using Paired Programming
