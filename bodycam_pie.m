e23b6a5e7905c8cee85a3bcb325967ad71d9a39e
michelleliu83 <79758219+michelleliu83@users.noreply.github.com>
2021-03-10 09:07:58 -0500

    Update bodycam_pie.m

--- bodycam_pie.m
+++ bodycam_pie.m
@@ -1,20 +1,31 @@
+% Visualization for officers wearing and not wearing a body camera when shooting occurred
+
+% Reading the table downloaded from GitHub
 T = readtable('fatal-police-shootings-data.csv');
+
+% Extracting the body_cam variable
 body_cam = T(:,14);
-% descriptive stats for bodycam 
+
+% Peforming descriptive stats for bodycam 
 bodycamcell = table2cell(body_cam);
 tbl_bodycam = tabulate(bodycamcell);
-counter = 0;
+% We use a counter and for loop and if statement to count for body camera 'False' and other
+counter = 0; 
 for ii = 1:length(bodycamcell) % xx = 1: length(numlist), numlist = list of bodycam variables
    string1 = bodycamcell(ii);
-   if strcmp('False', string1)
-       counter = counter + 1; % count number of false 
+   if strcmp('False', string1) 
+       counter = counter + 1; % Count number of false 
    else
-       counter = counter + 1; % count number of true
+       counter = counter + 1; % Count number of true
    end
 end
-% pie chart showing statistical data for body cam
+
+% Creating a pie chart that uses statistical data for wearing a body camera or not
 X = cell2mat(tbl_bodycam(:,2));
 labels = {'No Body Cam', 'Body Cam'};
 pie3(X,labels) % 3 D pie chart
 title('Body cams on policemen')
 
+% Written by: Sarah, 4 hours
+% Debugged by: Michelle, 4 hours
+% Collaborated with Pair Programming
